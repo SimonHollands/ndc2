292e37e2f88e24400cbc05796db4fd55a43bf0dd
Hollands <hollands@rand.org>
2017-04-07 16:31:13 -0700

    Setup commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='ndc2',
+      version='0.1',
+      description='Conver XXXX-XXX-XXX 10 Digit NDC to 11 Digit',
+      url='https://github.com/SimonHollands/ndc2.git',
+      author='Simon Hollands',
+      author_email='simon_hollands@hotmail.com',
+      license='None',
+      packages=['ndc2'],
+      zip_safe=False)
